ae883dc8f6f5df561afaa10f4c4d66cd9bbd49f1
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-11-04 15:30:12 +0100

    Backend

---
+++ src/main/java/com/swanhtetaungphyo/quickproject/QuickProjectApplication.java
@@ -0,0 +1,13 @@
+package com.swanhtetaungphyo.quickproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swanhtetaungphyo/quickproject/QuickProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swanhtetaungphyo.quickproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
